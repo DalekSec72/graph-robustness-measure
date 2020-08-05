84cb3cfbb2ecd2592ce69a8d25d1daceccada9d3
eeewq123 <rkakrnl0@gmail.com>
2020-08-06 07:16:40 +0900

    [FW002] Second-SP 출력

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -9,8 +9,12 @@ from graph_robustness_measure import floyd_warshall
 class FloydWarshallTests(unittest.TestCase):
     def test_floyd(self):
         g = graph.generate_random_graph(5, 0.7)
+        r = floyd_warshall.floyd(g)
+        sp = r[0]
+        s_sp = r[1]
         print(g.edges)
-        print(floyd_warshall.floyd(g))
+        print(sp)
+        print(s_sp)
 
 
 
