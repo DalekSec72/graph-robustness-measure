fb4d133234912e918aac1d808a6fa76e646a7a7d
Taehun.Kim <th6424@gmail.com>
2020-09-11 16:55:26 +0900

    [RM002] E 계산 (값 이상함)

--- graph_robustness_measure/efficiency_calculator.py
+++ graph_robustness_measure/efficiency_calculator.py
@@ -9,7 +9,27 @@ from .floyd_warshall import *
 
 
 # 플로이드 워셜 돌린 후 SP, second SP, ... 을 받아 E 계산.
-def calculate_efficiency(path):
+def sum_list_inverse(list):
+    res = 0
+    for i in list:
+        if type(i) == type(list):
+            res += sum_list_inverse(i)
+        else:
+            if i != 0:
+                res += 1/i # 역수로 바꿔 더해줌
+
+    return res
+
+def calculate_efficiency(costs, number_of_nodes):
+    #E(G) = 2/3 * 1/N(N-1)sigma(1/d_ij + 1/2*d_ij')
+    efficiency = 2 / ((3*number_of_nodes*(number_of_nodes-1)) * (sum_list_inverse(costs[0]) + 1/2*sum_list_inverse(costs[1])))
+    #print('n: %d, sp: %d, ssp: %d' %(number_of_nodes, sum_list_inverse(costs[0]), sum_list_inverse(costs[1])))
+
+    return efficiency
 
 
-#if __name__ == '__main__':
+if __name__ == '__main__':
+    graph = make_graph_with_edgelist()
+    costs = floyd(graph)
+    e = calculate_efficiency(costs, graph.number_of_nodes())
+    print(e)
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,18 +4,38 @@
 import unittest
 from graph_robustness_measure import graph
 from graph_robustness_measure import floyd_warshall
+from graph_robustness_measure import efficiency_calculator
 
 
 class FloydWarshallTests(unittest.TestCase):
     def test_floyd(self):
         g = graph.generate_random_graph(5, 0.7)
         r = floyd_warshall.floyd(g)
+
         sp = r[0]
         s_sp = r[1]
         print(g.edges)
         print(sp)
         print(s_sp)
 
+    def test_sum_list_inverse(self):
+        sample_list = [[2, 5, 20], [0, floyd_warshall.INF]]
+        expected = 15/20
+        actual = efficiency_calculator.sum_list_inverse(sample_list)
+
+        self.assertAlmostEqual(actual, expected)
+
+    def test_calculate_efficiency(self):
+        g = graph.generate_random_graph(5, 1)
+        r = floyd_warshall.floyd(g)
+        e = efficiency_calculator.calculate_efficiency(r, g.number_of_nodes())
+
+        print(g.edges)
+        print(r[0])
+        print(r[1])
+        print(e)
+
+        self.assertLessEqual(e, 1)
 
 
 if __name__ == '__main__':
