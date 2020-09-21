137bad5a2d828bf086154cf03addd5c50821846e
eeewq123 <rkakrnl0@gmail.com>
2020-09-21 23:07:55 +0900

    [RM003] 성능 개선 #1

--- graph_robustness_measure/floyd_warshall.py
+++ graph_robustness_measure/floyd_warshall.py
@@ -13,30 +13,44 @@ def floyd(graph):
     edges = graph.edges()
 
     # 무한대로 초기화.
-    cost = [[[INF for _ in range(number_of_nodes)] for _ in range(number_of_nodes)], [[INF for _ in range(number_of_nodes)] for _ in range(number_of_nodes)]]
+    cost = [[INF for _ in range(number_of_nodes)] for _ in range(number_of_nodes)]
+    cost_ssp = [[INF for _ in range(number_of_nodes)] for _ in range(number_of_nodes)]
 
     # 연결된 edge의 코스트를 1로.
     for edge in edges:
         start, end = int(edge[0]), int(edge[1])
-        cost[0][start][end] = 1
+        cost[start][end] = 1
+
+    for u in range(number_of_nodes):
+        cost[u][u] = 0
 
     # 플로이드 워셜.
     for k in range(number_of_nodes):
+        start_k = cost[k]
         for i in range(number_of_nodes):
+            start_i = cost[i]
+            start_i_ssp = cost_ssp[i]
+            itok = cost[i][k]
             for j in range(number_of_nodes):
-                # 자기 자신은 0.
-                if i == j:
-                    cost[0][i][j] = 0
-                    cost[1][i][j] = 0
                 # 더 짧은 경로로 변경.
-                else:
-                    # 최단경로 변경시 second-sp 값에 기존 최단경로 값 입력
-                    # 그 외 버려지는 값일 경우 second-sp 값과 비교하여 더 낮은 값 입력
-                    if cost[0][i][j] > (cost[0][i][k] + cost[0][k][j]):
-                        cost[1][i][j] = cost[0][i][j]
-                    elif cost[0][i][j] < (cost[0][i][k] + cost[0][k][j]):
-                        cost[1][i][j] = min(cost[1][i][j], cost[0][i][k] + cost[0][k][j])
+                # 최단경로 변경시 second-sp 값에 기존 최단경로 값 입력
+                # 그 외 버려지는 값일 경우 second-sp 값과 비교하여 더 낮은 값 입력
+                itoj = start_i[j]
+                distance = itok + start_k[j]
+                itoj_ssp = start_i_ssp[j]
 
-                    cost[0][i][j] = cost[0][j][i] = min(cost[0][i][j], cost[0][i][k] + cost[0][k][j])
+                if itoj > (distance):
+                    start_i_ssp[j] = itoj
+                elif itoj < (distance):
+                    start_i_ssp[j] = min(itoj_ssp, distance)
+
+                start_i[j] = cost[j][i] = min(itoj, distance)
+
+            cost[i] = start_i
+            cost_ssp[i] = start_i_ssp
+
+        cost[k] = start_k
+
+    result = [cost, cost_ssp]
 
     return cost
